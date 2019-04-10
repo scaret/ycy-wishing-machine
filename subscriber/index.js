4cc07f0a0f85f52d50047c1138a172db87b0e231
Derek Lu <scaret.in@gmail.com>
2019-04-10 23:36:10 +0800

    controls: false

--- subscriber/index.js
+++ subscriber/index.js
@@ -7,6 +7,10 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 const port = 8314;
 
+app.get("/", (req, res)=>{
+    res.redirect("/pages/subscriber/subscriber.html");
+});
+
 app.listen(port, ()=>{
     console.log(`http://localhost:${port}/pages/subscriber/subscriber.html`);
 });
